c2b0081d8b2dfe3704e1f510617fa4c380c59aee
SALMAN KHAN <51213314+sk10salman@users.noreply.github.com>
2020-03-24 13:58:20 +0530

    KNAPSACK UNBOUNDED

---
+++ rod_cutting.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int knap(int wt[],int vt[],int w,int n)
+{
+ int dp[n+1][w+1];
+   
+          for(int i=0;i<=n;i++)
+            { for(int j=0;j<=w;j++)
+               {
+				   if(i==0||j==0)
+				      dp[i][j]=0;
+                 else  if(wt[i-1]<=j)
+                   {
+                      dp[i][j]=max(vt[i-1] +  dp[i][(j-wt[i-1])]    ,  dp[i-1][j]);
+                   }
+                    else
+                      dp[i][j]=dp[i-1][j];
+               }
+			}
+               return dp[n][w];
+}
+int main() {
+
+	int t;
+    cout<<"enter test cases"
+	cin>>t;
+	while(t--)
+	{
+	    
+	    int n;
+        cout<<"\nenter length";
+	    cin>>n;
+	
+	    int wt[n],vt[n];
+	    for(int i=0;i<n;i++)
+	      wt[i]=i+1;
+          cout<<"\nenter elements";
+	     for(int i=0;i<n;i++)
+	       cin>>vt[i];
+	       cout<<knap(wt,vt,n,n)<<endl;
+	}
+	return 0;
+}
