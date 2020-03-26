6c66ce6e0cfc8fd9d8aa844e8bda3a50c900a187
SALMAN KHAN <51213314+sk10salman@users.noreply.github.com>
2020-03-26 19:04:21 +0530

    LCS DP

---
+++ LCS ALL VARIATION/lcsubsequence_dp.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lcs(string X,string Y,int n,int m)
+{
+  int dp[n+1][m+1];
+  for(int i=0;i<=m;i++)
+    dp[0][i]=0;
+  for(int i=0;i<=n;i++)
+    dp[i][0]=0;
+  for(int i=1;i<=n;i++)
+     for(int j=1;j<=m;j++)
+        {
+            if(X[i-1]==Y[j-1])
+               dp[i][j] = 1+ dp[i-1][j-1];
+             else
+               {
+                   dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+               }
+        }
+    return dp[n][m];
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+    string X,Y;
+    cin>>X>>Y;
+    cout<<lcs(X,Y,X.length(),Y.length())<<endl;
+    }
+}
